b1026387d96d9060a2b2542546465bbf4aacc2b3
diogo-fatec <diogo.kawaguchi.rosa@gmail.com>
2024-10-23 20:15:20 -0300

    :sparkles: feat: Tela de login finalizada

--- app/src/main/java/com/srcamelo_kotlin/ui/activities/MainActivity.kt
+++ app/src/main/java/com/srcamelo_kotlin/ui/activities/MainActivity.kt
@@ -9,18 +9,31 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.srcamelo_kotlin.R
+import com.srcamelo_kotlin.ui.components.ButtonWhite
 import com.srcamelo_kotlin.ui.components.InputLine
+import com.srcamelo_kotlin.ui.fonts.Montserrat
+import com.srcamelo_kotlin.ui.theme.DarkOrange
 import com.srcamelo_kotlin.ui.theme.LightOrange
 import com.srcamelo_kotlin.ui.theme.SrCamelo_KotlinTheme
 
@@ -49,9 +62,27 @@ fun MainScreen(
             .background(color = LightOrange)
             .fillMaxSize()){
         Image(painter = painterResource(id = R.drawable.logo), contentDescription = null)
-        InputLine(placeholder = "Email")
-        InputLine(placeholder = "Senha")
-        Text(text = "Não possuo uma conta")
+
+        Spacer(modifier = Modifier.height(44.dp))
+
+        var email by remember { mutableStateOf("") }
+        InputLine(placeholder = "Email", value = email, onValueChange = {email = it})
+
+        Spacer(modifier = Modifier.height(29.dp))
+
+        var password by remember { mutableStateOf("") }
+        InputLine(placeholder = "Senha", value = password, onValueChange = {password = it})
+
+        Spacer(modifier = Modifier.height(54.dp))
+
+        ButtonWhite("Entrar")
+        Spacer(modifier = Modifier.height(35.dp))
+        Text(text = "Não possuo uma conta",
+            fontFamily = Montserrat,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+            color = DarkOrange,
+            textDecoration = TextDecoration.Underline)
     }
 }
 
--- app/src/main/java/com/srcamelo_kotlin/ui/components/InputLine.kt
+++ app/src/main/java/com/srcamelo_kotlin/ui/components/InputLine.kt
@@ -4,11 +4,17 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.srcamelo_kotlin.ui.fonts.Montserrat
 import com.srcamelo_kotlin.ui.theme.DarkOrange
 import com.srcamelo_kotlin.ui.theme.SrCamelo_KotlinTheme
@@ -16,9 +22,15 @@ import com.srcamelo_kotlin.ui.theme.SrCamelo_KotlinTheme
 @Composable
 fun InputLine(
     placeholder: String,
+    value: String = "",
+    onValueChange: (String) -> Unit = {},
     modifier: Modifier = Modifier
 ){
-    TextField(value = "", onValueChange = {},
+    var textState by remember { mutableStateOf(TextFieldValue(value))}
+    TextField(value = textState,
+        onValueChange = {
+            textState = it
+            onValueChange(it.text)},
         label = { Text(placeholder, fontFamily = Montserrat, fontWeight = FontWeight.Medium) },
         colors = TextFieldDefaults.colors(
             focusedContainerColor = Color.Transparent,
@@ -26,15 +38,15 @@ fun InputLine(
             focusedIndicatorColor = DarkOrange,
             unfocusedIndicatorColor = DarkOrange
         ),
-        
-
+        textStyle = TextStyle(fontSize = 16.sp, fontFamily = Montserrat, fontWeight = FontWeight.Medium),
+        modifier = modifier
         )
 }
 
-@Preview(showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun InputPreview(){
     SrCamelo_KotlinTheme {
-        InputLine("Email")
+        InputLine(placeholder = "Email")
     }
 }
