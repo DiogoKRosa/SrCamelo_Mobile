87af511aff4b421beb5a2f98c5c63a809f199eaa
diogo-fatec <diogo.kawaguchi.rosa@gmail.com>
2024-10-23 21:31:56 -0300

    :sparkles: feat: Tela de escolha para cadastro feita

--- app/src/main/java/com/srcamelo_kotlin/ui/activities/ChooseAccountActivity.kt
+++ app/src/main/java/com/srcamelo_kotlin/ui/activities/ChooseAccountActivity.kt
@@ -1,7 +1,5 @@
 package com.srcamelo_kotlin.ui.activities
 
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,17 +9,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -32,30 +24,19 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.srcamelo_kotlin.R
 import com.srcamelo_kotlin.ui.components.ButtonWhite
-import com.srcamelo_kotlin.ui.components.InputLine
-import com.srcamelo_kotlin.ui.components.SpecialText
 import com.srcamelo_kotlin.ui.fonts.Montserrat
 import com.srcamelo_kotlin.ui.theme.DarkOrange
 import com.srcamelo_kotlin.ui.theme.LightOrange
 import com.srcamelo_kotlin.ui.theme.SrCamelo_KotlinTheme
 
-class MainActivity : ComponentActivity() {
+class ChooseAccountActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SrCamelo_KotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(
-                        chooseAccount = {
-                            startActivity(
-                                Intent(
-                                    this,
-                                    ChooseAccountActivity::class.java
-                                )
-                            )
-                        }
-                    )
+                    ChooseAccountScreen()
                 }
             }
         }
@@ -63,47 +44,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(
-    modifier: Modifier = Modifier,
-    chooseAccount: () -> Unit = {}
-){
+fun ChooseAccountScreen(){
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        modifier = Modifier
             .background(color = LightOrange)
-            .fillMaxSize()){
+            .fillMaxSize()
+    ) {
         Image(painter = painterResource(id = R.drawable.logo), contentDescription = null)
 
-        Spacer(modifier = Modifier.height(44.dp))
+        Spacer(modifier = Modifier.height(83.dp))
 
-        var email by remember { mutableStateOf("") }
-        InputLine(placeholder = "Email", value = email, onValueChange = {email = it})
+        ButtonWhite("Consumidor")
 
-        Spacer(modifier = Modifier.height(29.dp))
+        Spacer(modifier = Modifier.height(50.dp))
 
-        var password by remember { mutableStateOf("") }
-        InputLine(placeholder = "Senha", value = password, onValueChange = {password = it})
+        ButtonWhite("Vendedor")
 
-        Spacer(modifier = Modifier.height(54.dp))
-
-        ButtonWhite("Entrar")
         Spacer(modifier = Modifier.height(35.dp))
-        SpecialText(text = "Não possuo uma conta",
-            onClick = {
-                chooseAccount()
-            })
-    }
+
+        Text(text = "Já possuo uma conta",
+            fontFamily = Montserrat,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+            color = DarkOrange,
+            textDecoration = TextDecoration.Underline)
+}
 }
 
 @Preview(showSystemUi = true)
 @Composable
-fun MainPreview() {
+fun PreviewScreen(){
     SrCamelo_KotlinTheme {
-        Scaffold(modifier = Modifier.fillMaxSize(),
-            ){ innerPadding ->
-            MainScreen(modifier = Modifier
-                .fillMaxSize())
+        Scaffold { innerpadding ->
+            ChooseAccountScreen()
         }
     }
 }
--- app/src/main/java/com/srcamelo_kotlin/ui/activities/LoginActivity.kt
+++
@@ -1,2 +0,0 @@
-package com.srcamelo_kotlin.ui.activities
-
