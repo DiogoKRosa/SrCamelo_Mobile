159146c52ed04133c76b4079760e7a6fbdd140d5
Diogo Kawaguchi Rosa <diogo.kawaguchi.rosa@gmail.com>
2025-05-10 23:15:50 -0300

    :bug: fix: Consertado problema ao enviar dados a api

--- app/src/main/java/com/srcamelo_kotlin/model/LocationModel.kt
+++ app/src/main/java/com/srcamelo_kotlin/model/LocationModel.kt
@@ -3,7 +3,7 @@ package com.srcamelo_kotlin.model
 import com.google.gson.annotations.SerializedName
 
 data class LocationModel(
-    @SerializedName("user_id")
+    @SerializedName("userId")
     val userId: String,
     @SerializedName("latitude")
     val latitude: Double,
